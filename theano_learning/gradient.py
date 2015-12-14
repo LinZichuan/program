494a19369fca91b43671eef038f5bb6e9782d83f
LinZichuan <linzichuan12@163.com>
2015-12-14 10:08:24 +0800

    Theano logistic gradient test

--- theano_learning/gradient.py
+++ theano_learning/gradient.py
@@ -7,3 +7,9 @@ gy = T.grad(y, x)
 f = theano.function([x], gy)
 print f(4)
 
+x = T.dmatrix('x')
+s = T.sum(1 / (1 + T.exp(-x)))
+gs = T.grad(s, x)
+dlogis = theano.function([x], gs)
+print dlogis([[0, 1], [-1, -2]])
+
