690c7ec3c92389093698eadb90532be39f6877ec
LinZichuan <linzichuan12@163.com>
2015-12-14 11:19:34 +0800

    Theano computing Jacobian

--- theano_learning/gradient.py
+++ theano_learning/gradient.py
@@ -13,3 +13,8 @@ gs = T.grad(s, x)
 dlogis = theano.function([x], gs)
 print dlogis([[0, 1], [-1, -2]])
 
+x = T.dvector('x')
+y = x ** 2
+J, updates = theano.scan(lambda i, y,x : T.grad(y[i], x), sequences=T.arange(y.shape[0]), non_sequences=[y,x])
+f = theano.function([x], J, updates=updates)
+print f([4, 4])
